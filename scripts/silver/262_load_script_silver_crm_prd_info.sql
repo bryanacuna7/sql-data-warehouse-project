5e5a4c9ae0fdc77ece7cde7064dc65a434657ed9
Bryan Acuña <161381023+bryanacuna7@users.noreply.github.com>
2025-06-04 21:57:25 -0600

    Update 262_load_script_silver_crm_prd_info.sql

--- scripts/silver/262_load_script_silver_crm_prd_info.sql
+++ scripts/silver/262_load_script_silver_crm_prd_info.sql
@@ -1,19 +1,3 @@
--- Drop the table if it already exists to ensure a clean slate
-DROP TABLE IF EXISTS silver_crm_prd_info;
-
--- Create the 'silver_crm_prd_info' table with product details
-CREATE TABLE silver_crm_prd_info (
-	prd_id INT,
-	cat_id NVARCHAR(50),        -- Category ID extracted from product key
-	prd_key NVARCHAR(50),       -- Unique product key (modified version)
-	prd_nm NVARCHAR(50),        -- Product name
-	prd_cost INT,               -- Product cost, defaults to 0 if null
-	prd_line NVARCHAR(50),      -- Product line (e.g., Mountain, Road)
-	prd_start_dt DATE,          -- Product start date
-	prd_end_dt DATE,            -- Product end date (calculated using LEAD)
-	dwh_create_date DATETIME DEFAULT CURRENT_TIMESTAMP -- Record creation timestamp
-);
-
 -- Insert transformed and cleaned data from 'bronze_crm_prd_info' into 'silver_crm_prd_info'
 INSERT INTO silver_crm_prd_info (
 	prd_id, cat_id, prd_key, prd_nm, prd_cost, prd_line, prd_start_dt, prd_end_dt
